6750a783b015672fcca394b899b72fdda04ceed1
Erick Delfin <erickdelfinmolina@gmail.com>
2018-03-05 22:13:44 -0700

    Set up files for Heroku deploy

--- tap_universiada/settings.py
+++ tap_universiada/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+# TODO : Set up DEBUG as env var and only execute this if not DEBUG
+django_heroku.settings(locals())
