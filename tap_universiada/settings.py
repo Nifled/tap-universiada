c48c04c13eea2c32708adc1ee0a14f98984487d1
Abraham Hurtado <arhc.hc@gmail.com>
2018-03-07 21:44:06 -0700

    Added support for template rendering and splitted view definition

--- tap_universiada/settings.py
+++ tap_universiada/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tap_universiada.views'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tap_universiada.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ tap_universiada/views/index.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+  hola = "Hola mundo"
+
+  return render(request, 'index.html', {
+    'hola': hola
+  })
