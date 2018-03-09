396143fa7057813363113ff888119a55789e06e9
Abraham Hurtado <arhc.hc@gmail.com>
2018-03-09 12:40:19 -0700

    Added model Participante

--- tap_universiada/models.py
+++ tap_universiada/models.py
@@ -2,3 +2,28 @@ from django.db import models
 
 class Disciplina(models.Model):
   nombre = models.CharField(max_length=30)
+
+class Participante(models.Model):
+  nombres = models.CharField(max_length=255)
+  apellido_paterno = models.CharField(max_length=255)
+  apellido_materno = models.CharField(max_length=255)
+  estado = models.BooleanField(default=True)
+  ultima_comida = models.BooleanField(default=False)
+  institucion = models.CharField(max_length=255)
+  TIPOS_CHOICES = (
+    ('deportista', 'DEPORTISTA'),
+    ('entrenador', 'ENTRENADOR'),
+    ('juez', 'JUEZ'),
+    ('delegado', 'DELEGADO'),
+    ('coordinador', 'COORDINADOR'),
+    ('comisionado-tecnico', 'COMISIONADO TÉCNICO'),
+    ('medico', 'MÉDICO'),
+    ('comite-organizador', 'COMITÉ ORGANIZADOR'),
+  )
+  tipos = models.CharField(
+    max_length=19,
+    choices=TIPOS_CHOICES,
+    default='deportista'
+  )
+
+  
