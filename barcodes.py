f08b6697958cdd72c6f88b993b26ceeb1ea53a6f
Erick Delfin <erickdelfinmolina@gmail.com>
2018-03-13 22:34:50 -0700

    Refactor barcode generator and make file runnable

--- barcodes.py
+++ barcodes.py
@@ -1,29 +1,42 @@
+# TODO : Move somewhere else. e.g. in comidas/ dir
+
 import barcode
 from barcode.writer import ImageWriter
 
-def generate(size):
-    '''Generar lista de barcodes (strings) de tamaño `size`.'''
 
-    seed = '00001234000'    
+def generate_barcode_list(size):
+    """Generar lista de barcodes (strings) de tamaño `size`."""
+
+    seed = '00001234000'
     barcode_list = []
-    for i in range(1,size+1):
+    for i in range(1, size + 1):
         code = str(i) + seed
-        bcode = barcode.get('ean', code[0:12])
-        barcode_list.append(bcode.get_fullcode())
+        b_code = barcode.get('ean', code[0:12])
+        barcode_list.append(b_code.get_fullcode())
 
     return barcode_list
 
-# checar que ningún código se repita
+
+# checar que ningún código se repita, etc
 # print(len(set(generate(800)))) # ok
 
+
 def generate_png(n):
-    '''Generar `n` códigos de barra (imágenes).'''
+    """Generar `n` códigos de barra (imágenes)."""
 
-    seed = '00001234000'    
-    for i in range(1,size+1):
+    seed = '00001234000'
+    for i in range(1, n + 1):
         code = str(i) + seed
-        bcode = barcode.get('ean', code[0:12], writer=ImageWriter())
+        b_code = barcode.get('ean', code[0:12], writer=ImageWriter())
 
-        filename = 'barcode_' + bcode.get_fullcode()
-        bcode.default_writer_options['quiet_zone'] = 1.0
-        bcode.save(filename)
+        filename = 'barcode_' + b_code.get_fullcode()
+        b_code.default_writer_options['quiet_zone'] = 1.0
+        b_code.save(filename)
+
+
+if __name__ == "__main__":
+    # Para probar, correr solo este archivo e.g.
+    # `docker-compose -f compose-local.yml run web python barcodes.py`
+    b_list = generate_barcode_list(3)
+    print(b_list)
+    generate_png(3)
