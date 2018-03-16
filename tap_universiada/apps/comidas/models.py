4bac3fc5d6737b6e46fbeadd2f1055b4e3d302fa
Juanets <jdgs.gt@gmail.com>
2018-03-15 20:49:16 -0600

    Agregados campos de barcode

--- tap_universiada/apps/comidas/models.py
+++ tap_universiada/apps/comidas/models.py
@@ -4,6 +4,10 @@ from .choices import *
 
 import uuid
 
+from . import barcodes
+
+from random import randint
+
 # ----------------------------------------------------------------------
 # **************************** Disciplina ******************************
 # ----------------------------------------------------------------------
@@ -26,6 +30,8 @@ class Equipo(models.Model):
 # ----------------------------------------------------------------------
 class Participante(models.Model):
     uuid = models.UUIDField(blank=True, default=uuid.uuid4,editable=False, unique=True)
+    barcode = models.CharField(max_length=13, blank=True, editable=False, unique=True)
+    barcode_link = models.CharField(max_length=35, blank=True, editable=False, unique=True)
     nombres = models.CharField(max_length=85)
     apellido_p = models.CharField(max_length=45, verbose_name='Apellido Paterno')
     apellido_m = models.CharField(max_length=45, verbose_name='Apellido Materno')
@@ -44,6 +50,11 @@ class Participante(models.Model):
         return '{} - {}, Estatus: {} {}'\
             .format(self.apellido_p, self.disciplina.nombre, self.estatus, self.get_tipo_display())
 
+    def save(self, *args, **kwargs):
+        barcode_str, barcode_link = barcodes.generate_one(str(randint(100000000000,999999999999)))
+        self.barcode = barcode_str
+        self.barcode_link = barcode_link
+        super(Participante, self).save(*args, **kwargs)
 
 # ----------------------------------------------------------------------
 # ****************************** Comida ********************************
