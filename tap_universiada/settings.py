544946af969ff661d634c725dc67c06ec6a711e3
Erick Delfin <erickdelfinmolina@gmail.com>
2018-03-16 15:34:50 -0700

    Change timezone to UTC

--- tap_universiada/settings.py
+++ tap_universiada/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'es-mx'
-TIME_ZONE = 'America/Phoenix'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
